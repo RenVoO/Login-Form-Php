9fbf97c2e39090d69542e8527014935d1e657d51
RenVo <ahmadfahrifauzan@gmail.com>
2024-05-20 22:50:23 +0700

    Create UserPage.php

---
+++ UserPage.php
@@ -0,0 +1,27 @@
+<?php 
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="konten">
+            <h3>Hi, <span>User</span></h3>
+            <h1>Welcome</h1>
+            <p>ini adalah halaman user</p>
+            <a href="Login.php" class="tombol">Login</a>
+            <a href="register.php" class="tombol">Register</a>
+            <a href="Logout.php" class="tombol">Logout</a>
+        </div>
+    </div>
+</body>
+</html>
