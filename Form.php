c18b90901bb07086156da8f936f43267c294a708
RenVo <ahmadfahrifauzan@gmail.com>
2024-05-21 10:13:09 +0700

    Create Form.php

---
+++ Form.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Halaman Utama</title>
+    <link rel="stylesheet" href="Form.css">
+</head>
+<body>
+    <h1>Selamat Datang di Aplikasi Pendaftaran</h1>
+    <br>
+    <p><a href="register.php">Daftar</a> | <a href="login.php">Login</a></p>
+</body>
+</html>
